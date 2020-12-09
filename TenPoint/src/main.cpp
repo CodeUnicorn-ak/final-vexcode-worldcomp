2e294ed18a3d366a271f60ccf30171fb28c59c00
Lucky <anvitareddy@icloud.com>
2020-12-09 09:26:03 -0500

    Before first comp, pretty successful, Wednesday 12/9

--- TenPoint/src/main.cpp
+++ TenPoint/src/main.cpp
@@ -60,7 +60,7 @@ void conDeadZone() {
     rightJoystick = 0;
   }
   // Self Correcting Drive
-  if (fabs(rightJoystick - leftJoystick) < 40) {
+  if (fabs(rightJoystick - leftJoystick) < 45) {
     rightJoystick = Joystick;
     leftJoystick = Joystick;
   }
@@ -73,10 +73,10 @@ void conDeadZone() {
 }
 //
 void stopBase() {
-  leftDrive.stop(brakeType::hold);
-  rightDrive.stop(brakeType::hold);
-  leftDrive.stop(brakeType::hold);
-  rightDrive.stop(brakeType::hold);
+  leftDrive.stop(brakeType::coast);
+  rightDrive.stop(brakeType::coast);
+  leftDrive.stop(brakeType::coast);
+  rightDrive.stop(brakeType::coast);
 }
 void drive(int velo, int distance) {
   Drivetrain.setDriveVelocity(velo, percent);
@@ -92,10 +92,7 @@ void velo(int velo) {
   Lintake.setVelocity(velo, pct);
   Rintake.setVelocity(velo, pct);
 }
-void sleep(int time){
-vex::task::sleep(time);
-
-}
+void sleep(int time) { vex::task::sleep(time); }
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -116,7 +113,7 @@ void pre_auton(void) {
   leftDrive.resetRotation();
   rightDrive.resetPosition();
   rightDrive.resetRotation();
-    Rintake.resetPosition();
+  Rintake.resetPosition();
   Lintake.resetRotation();
   Rintake.resetPosition();
   Lintake.resetRotation();
@@ -124,7 +121,6 @@ void pre_auton(void) {
   FRoller.resetRotation();
   BRoller.resetPosition();
   FRoller.resetRotation();
-
 }
 
 /*---------------------------------------------------------------------------*/
@@ -138,89 +134,232 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  //flip-out and intake
-  velo(100);
+  // flip-out
+  drive(30, 3);
+  // drive(10, -7);
+  rightDrive.setVelocity(15, pct);
+  leftDrive.setVelocity(15, pct);
+  rightDrive.spin(directionType::rev);
+  leftDrive.spin(directionType::rev);
+  wait(1500, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  Lintake.setVelocity(70, pct);
+  Rintake.setVelocity(70, pct);
   Lintake.spin(directionType::fwd);
   Rintake.spin(directionType::fwd);
-  BRoller.spin(directionType::fwd);
-  FRoller.spin(directionType::fwd);
-  //move forward to intake 1st ball plus preload
-  drive(50, 40);
-  Rintake.stop(brakeType::hold);
-  Lintake.stop(brakeType::hold);
-  BRoller.stop(brakeType::hold);
-  FRoller.stop(brakeType::hold);
-  /*
-  //end of stage 1
-  //turn left 90
-  turn(25, -90);
-  //move forward
-  drive(50, 12);
-  //turn left 45
-  turn(25, -45);
-  //move forward towards tower
-  drive(30, 40);
-//put ball in
-  velo(100);
+  wait(500, msec);
+  // drive fwd
+  drive(25, 11);
+
+  Drivetrain.stop();
+
+  Drivetrain.setTurnVelocity(20, pct);
+  Drivetrain.turnFor(-84, degrees);
+
+  Drivetrain.stop();
+
+  drive(40, 20);
+  // Drivetrain.turnFor(90, degrees);
+
+  // drive(40, 10);
+
+  Lintake.setVelocity(100, pct);
+  Rintake.setVelocity(100, pct);
   Lintake.spin(directionType::fwd);
   Rintake.spin(directionType::fwd);
-  BRoller.spin(directionType::fwd);
-  FRoller.spin(directionType::fwd);
-  wait(3000, msec);
-  Rintake.stop(brakeType::hold);
-  Lintake.stop(brakeType::hold);
-  BRoller.stop(brakeType::hold);
-  FRoller.stop(brakeType::hold);
-  //move backward to get into position 
-  drive(45, -20);
-  //turn towards wall
-  turn(25, 225);
-  //move backward towards wall and realign
-  drive(25, -30);
-  wait(1000, msec);
-  //move towards next balls
-  drive(50, 52);
-  //turn towards 2 new balls
-  turn(25, 90);
-  //drive forward while intaking
-  velo(50);
-  Lintake.spin(directionType::fwd);
-  Rintake.spin(directionType::fwd);
-  BRoller.spin(directionType::fwd);
-  FRoller.spin(directionType::fwd);
-  drive(50,54);
-  //turn left
-  turn(25,-90);
-  //move towards tower and put balls in
-  drive(35, 45);
+  // turn left 90
+  Drivetrain.setTurnVelocity(10, percent);
+  Drivetrain.turnFor(-36, degrees);
+
+  Lintake.stop();
+  Rintake.stop();
+
+  rightDrive.setVelocity(50, pct);
+  leftDrive.setVelocity(50, pct);
+  rightDrive.spin(directionType::fwd);
+  leftDrive.spin(directionType::fwd);
   wait(2000, msec);
-  Rintake.stop(brakeType::hold);
-  Lintake.stop(brakeType::hold);
-  BRoller.stop(brakeType::hold);
-  FRoller.stop(brakeType::hold);
-  //drive backwards
-  drive(25, -12);
-  //turn right towards tower
-  turn(25, 90);
-  //move forward to tower
-  drive(50, 50);
-  //45 degree turn left
-  turn(25, -45);
-  //move forward and put ball in
-  velo(100);
-  Lintake.spin(directionType::fwd);
-  Rintake.spin(directionType::fwd);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  // put in ball
+  FRoller.setVelocity(100, pct);
+  BRoller.setVelocity(100, pct);
   BRoller.spin(directionType::fwd);
   FRoller.spin(directionType::fwd);
-  drive(30,15);
-  wait(2000,msec);
-  Rintake.stop(brakeType::hold);
-  Lintake.stop(brakeType::hold);
-  BRoller.stop(brakeType::hold);
-  FRoller.stop(brakeType::hold);
-  //drive back
-  drive(30, -30);
-  */
+  // drive(50, 3);
+  wait(1000, msec);
+  BRoller.stop();
+  FRoller.stop();
+
+  rightDrive.setVelocity(15, pct);
+  leftDrive.setVelocity(15, pct);
+  rightDrive.spin(directionType::rev);
+  leftDrive.spin(directionType::rev);
+  wait(2300, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  Drivetrain.setTurnVelocity(20, percent);
+  Drivetrain.turnFor(125, degrees);
+
+  BRoller.stop();
+  FRoller.stop();
+  // turn back
+  rightDrive.setVelocity(15, pct);
+  leftDrive.setVelocity(15, pct);
+  rightDrive.spin(directionType::rev);
+  leftDrive.spin(directionType::rev);
+  wait(2550, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  // moving ball up
+  FRoller.setVelocity(55, pct);
+  BRoller.setVelocity(55, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  Lintake.setVelocity(100, pct);
+  Rintake.setVelocity(100, pct);
+  Lintake.spin(directionType::fwd);
+  Rintake.spin(directionType::fwd);
+  wait(400, msec);
+  BRoller.stop();
+  FRoller.stop();
+
+  // moving to next ball
+  Lintake.setVelocity(70, pct);
+  Rintake.setVelocity(70, pct);
+  drive(25, 55);
+  drive(20, -10);
+
+  Drivetrain.setTurnVelocity(20, percent);
+  Drivetrain.turnFor(-88, degrees);
+
+  Lintake.stop();
+  Rintake.stop();
+
+  // move towards tower
+  rightDrive.setVelocity(30, pct);
+  leftDrive.setVelocity(30, pct);
+  rightDrive.spin(directionType::fwd);
+  leftDrive.spin(directionType::fwd);
+  wait(2000, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  FRoller.setVelocity(100, pct);
+  BRoller.setVelocity(100, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  Lintake.setVelocity(5, pct);
+  Rintake.setVelocity(5, pct);
+  Lintake.spin(directionType::rev);
+  Rintake.spin(directionType::rev);
+  wait(800, msec);
+  BRoller.stop();
+  FRoller.stop();
+  drive(20, -7);
+  Lintake.stop();
+  Rintake.stop();
+
+  FRoller.setVelocity(100, pct);
+  BRoller.setVelocity(100, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  Lintake.setVelocity(100, pct);
+  Rintake.setVelocity(100, pct);
+  Lintake.spin(directionType::fwd);
+  Rintake.spin(directionType::fwd);
+  wait(300, msec);
+  BRoller.stop();
+  FRoller.stop();
+  Lintake.stop();
+  Rintake.stop();
+
+  drive(20, 7);
+
+  FRoller.setVelocity(100, pct);
+  BRoller.setVelocity(100, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  wait(2000, msec);
+  BRoller.stop();
+  FRoller.stop();
+
+  drive(20, -16);
+  Drivetrain.setTurnVelocity(20, pct);
+  Drivetrain.turnFor(85, degrees);
+
+  Drivetrain.stop();
+
+  Lintake.setVelocity(75, pct);
+  Rintake.setVelocity(75, pct);
+  Lintake.spin(directionType::fwd);
+  Rintake.spin(directionType::fwd);
+  wait(500, msec);
+
+  // drive fwd
+  drive(20, 40);
+  Lintake.stop();
+  Rintake.stop();
+  rightDrive.setVelocity(30, pct);
+  leftDrive.setVelocity(30, pct);
+  rightDrive.spin(directionType::fwd);
+  leftDrive.spin(directionType::fwd);
+  wait(1500, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  Lintake.setVelocity(75, pct);
+  Rintake.setVelocity(75, pct);
+  Lintake.spin(directionType::fwd);
+  Rintake.spin(directionType::fwd);
+  drive(30, -17);
+  Drivetrain.setTurnVelocity(25, pct);
+  Drivetrain.turnFor(-41, degrees);
+
+  FRoller.setVelocity(45, pct);
+  BRoller.setVelocity(45, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  Lintake.setVelocity(100, pct);
+  Rintake.setVelocity(100, pct);
+  Lintake.spin(directionType::fwd);
+  Rintake.spin(directionType::fwd);
+  wait(400, msec);
+  BRoller.stop();
+  FRoller.stop();
+ 
+Lintake.stop();
+Rintake.stop();
+
+  rightDrive.setVelocity(25, pct);
+  leftDrive.setVelocity(25, pct);
+  rightDrive.spin(directionType::fwd);
+  leftDrive.spin(directionType::fwd);
+  wait(2300, msec);
+  rightDrive.stop();
+  leftDrive.stop();
+
+  FRoller.setVelocity(100, pct);
+  BRoller.setVelocity(100, pct);
+  BRoller.spin(directionType::fwd);
+  FRoller.spin(directionType::fwd);
+  // drive(50, 3);
+  wait(1500, msec);
+  BRoller.stop();
+  FRoller.stop();
+
+  rightDrive.setVelocity(15, pct);
+  leftDrive.setVelocity(15, pct);
+  rightDrive.spin(directionType::rev);
+  leftDrive.spin(directionType::rev);
+  wait(2300, msec);
+  rightDrive.stop();
+  leftDrive.stop();
 }
 
 /*---------------------------------------------------------------------------*/
